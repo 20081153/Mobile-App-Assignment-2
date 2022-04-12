1e86fa72094f3416ef53a21ecdbbf992d6df8d72
DESKTOP-ENPPS3E\bcega <bcegan5@gmail.com>
2022-04-12 20:23:54 +0100

    Initial App

---
+++ app/src/main/java/ie/wit/findafive/ui/add_match/AddMatchViewModel.kt
@@ -0,0 +1,13 @@
+package ie.wit.findafive.ui.add_match
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddMatchViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is add match Fragment"
+    }
+    val text: LiveData<String> = _text
+}
