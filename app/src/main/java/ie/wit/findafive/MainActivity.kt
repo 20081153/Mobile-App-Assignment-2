453fa051e8cc5898ac9870ea636ce7b53a1acb72
DESKTOP-ENPPS3E\bcega <bcegan5@gmail.com>
2022-04-12 20:30:28 +0100

    mmm code

--- app/src/main/java/ie/wit/findafive/MainActivity.kt
+++ app/src/main/java/ie/wit/findafive/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import ie.wit.findafive.databinding.ActivityMainBinding
-
+//mmm code
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
