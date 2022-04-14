fe3d94745054f00cd6d5b4f271268b52ed178a21
DESKTOP-ENPPS3E\bcega <bcegan5@gmail.com>
2022-04-14 18:31:41 +0100

    New color Scheme

--- app/src/main/java/ie/wit/findafive/ui/add_match/AddMatchViewModel.kt
+++ app/src/main/java/ie/wit/findafive/ui/add_match/AddMatchViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class AddMatchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is add match Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
 }
